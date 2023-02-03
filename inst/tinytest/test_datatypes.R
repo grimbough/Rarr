eece86c83668ec9ce1a70a7027ba666fca9a0c31
Mike Smith <grimbough@gmail.com>
2023-02-03 16:00:50 +0100

    More unit tests

--- inst/tinytest/test_datatypes.R
+++ inst/tinytest/test_datatypes.R
@@ -1,13 +1,23 @@
 
 expect_equal(.check_datatype(data_type = "integer"), "<i4")
 expect_equal(.check_datatype(data_type = "double"), "<f8")
-expect_equal(.check_datatype(data_type = "character"), "|S")
+expect_equal(.check_datatype(data_type = "character", nchar = "10"), "|S10")
 
 expect_equal(.check_datatype(fill_value = 4L), "<i4")
 expect_equal(.check_datatype(fill_value = 4), "<f8")
-expect_equal(.check_datatype(fill_value = "Test"), "|S")
+expect_equal(.check_datatype(fill_value = "Test", nchar = 8), "|S8")
 
-## data_type should win when both are supplied and incompatible
+## data_type should have precedence when both are supplied and incompatible
 expect_equal(.check_datatype(data_type = "integer", fill_value = 4), "<i4")
 expect_equal(.check_datatype(data_type = "double", fill_value = "Test"), "<f8")
-expect_equal(.check_datatype(data_type = "character", fill_value = 4L), "|S")
+expect_equal(.check_datatype(data_type = "character", fill_value = 4L, nchar = 4), "|S4")
+
+expect_error(.check_datatype(data_type = "not-a-data-type"))
+expect_error(.check_datatype(data_type = "<i2"))
+expect_error(.check_datatype(data_type = list(1:10)))
+expect_error(.check_datatype(data_type = raw(10)))
+
+expect_error(.check_datatype(data_type = "|S"), pattern = "The 'nchar' argument must be provided")
+expect_error(.check_datatype(fill_value = "Testing"), pattern = "The 'nchar' argument must be provided")
+expect_error(.check_datatype(fill_value = "Testing", nchar = -4), pattern = "The 'nchar' argument must be provided")
+
---
+++ inst/tinytest/test_roundtrip.R
@@ -0,0 +1,12 @@
+
+
+path <- file.path(tempdir(), "empty_100.zarr")
+expect_silent(res <- create_empty_zarr_array(path = path, 
+                                             dim = 100, chunk_dim = 10, data_type = "integer", fill_value = 100L))
+expect_true(res)
+expect_identical(read_zarr_array(path), array(100L, dim = c(100)))
+
+
+x <- array(runif(n = 1000, min = -10, max = 10), dim = c(10, 20, 5))
+path <- file.path(tempdir(), "-10_10.zarr")
+expect_silent(res <- write_zarr_array(x = x, path = path, chunk_dim = c(2, 5, 1)))
