31afac972bac0c6cd5ea3ff2de9f469237d9e0a3
Mike Smith <grimbough@gmail.com>
2023-02-04 13:56:10 +0100

    Update unit tests

--- inst/tinytest/test_datatypes.R
+++ inst/tinytest/test_datatypes.R
@@ -1,16 +1,16 @@
 
-expect_equal(.check_datatype(data_type = "integer"), "<i4")
-expect_equal(.check_datatype(data_type = "double"), "<f8")
-expect_equal(.check_datatype(data_type = "character", nchar = "10"), "|S10")
+expect_equal(.check_datatype(data_type = "integer")[["data_type"]], "<i4")
+expect_equal(.check_datatype(data_type = "double")[["data_type"]], "<f8")
+expect_equal(.check_datatype(data_type = "character", nchar = "10")[["data_type"]], "|S10")
 
-expect_equal(.check_datatype(fill_value = 4L), "<i4")
-expect_equal(.check_datatype(fill_value = 4), "<f8")
-expect_equal(.check_datatype(fill_value = "Test", nchar = 8), "|S8")
+expect_equal(.check_datatype(fill_value = 4L)[["data_type"]], "<i4")
+expect_equal(.check_datatype(fill_value = 4)[["data_type"]], "<f8")
+expect_equal(.check_datatype(fill_value = "Test", nchar = 8)[["data_type"]], "|S8")
 
 ## data_type should have precedence when both are supplied and incompatible
-expect_equal(.check_datatype(data_type = "integer", fill_value = 4), "<i4")
-expect_equal(.check_datatype(data_type = "double", fill_value = "Test"), "<f8")
-expect_equal(.check_datatype(data_type = "character", fill_value = 4L, nchar = 4), "|S4")
+expect_equal(.check_datatype(data_type = "integer", fill_value = 4)[["data_type"]], "<i4")
+expect_equal(.check_datatype(data_type = "double", fill_value = "Test")[["data_type"]], "<f8")
+expect_equal(.check_datatype(data_type = "character", fill_value = 4L, nchar = 4)[["data_type"]], "|S4")
 
 expect_error(.check_datatype(data_type = "not-a-data-type"))
 expect_error(.check_datatype(data_type = "<i2"))
