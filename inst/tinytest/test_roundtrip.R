9e91bc29e664b292fc8117476133609566cfa94e
Mike Smith <grimbough@gmail.com>
2023-02-08 09:52:42 +0100

    Updating coverage workflow

--- inst/tinytest/test_roundtrip.R
+++ inst/tinytest/test_roundtrip.R
@@ -10,3 +10,10 @@ x <- array(runif(n = 1000, min = -10, max = 10), dim = c(10, 20, 5))
 path <- file.path(tempdir(), "-10_10.zarr")
 expect_silent(res <- write_zarr_array(x = x, zarr_array_path = path, chunk_dim = c(2, 5, 1)))
 expect_identical(read_zarr_array(path), x)
+
+## testing blosc compression
+path <- tempfile()
+expect_silent(res <- write_zarr_array(x = x, zarr_array_path = path, 
+                                      chunk_dim = c(2, 5, 1), 
+                                      compressor = use_blosc()))
+expect_identical(read_zarr_array(path), x)
