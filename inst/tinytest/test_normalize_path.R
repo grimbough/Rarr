2b4d15c7b0e7a5659c70368a02b749eca3d63a8e
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-21 18:47:05 +0200

    Test actual paths with a regex

--- inst/tinytest/test_normalize_path.R
+++ inst/tinytest/test_normalize_path.R
@@ -11,10 +11,7 @@ paths <- c(
   "../foo/bar/baz.zarr",
   "./foo/bar/baz.zarr",
   ".//foo///bar/baz.zarr",
-  # Actually existing paths
-  "~/baz.zarr",
   "baz.zarr",
-  file.path(tempdir(), "foo", "/bar//", "baz.zarr"),
   # URLs
   "https://s3.foo.com/bar/baz.zarr"
 )
@@ -29,9 +26,7 @@ normalized_paths <- c(
   "../foo/bar/baz.zarr/",
   "./foo/bar/baz.zarr/",
   "./foo/bar/baz.zarr/",
-  file.path(path.expand("~"), "baz.zarr", ""),
   "baz.zarr/",
-  file.path(tempdir(), "foo", "bar", "baz.zarr", ""),
   "https://s3.foo.com/bar/baz.zarr/"
 )
 
@@ -39,5 +34,22 @@ for(i in seq_along(paths)) {
   expect_identical(
     Rarr:::.normalize_array_path(paths[i]),
     normalized_paths[i]
+  )
+}
+
+# Paths that actually exist on our testing filesystem
+actual_paths <- c(
+  "~/baz.zarr",
+  file.path(tempdir(), "foo", "/bar//", "baz.zarr")
+)
+
+# Specification states:
+# After normalization, if splitting a logical path by the “/” character results
+# in any path segment equal to the string “.” or the string “..” then an error
+# MUST be raised.
+for (i in seq_along(actual_paths)) {
+  expect_match(
+    Rarr:::.normalize_array_path(actual_paths[i]),
+    "/([^(.|..)]+/)*baz.zarr/$"
   )
 }
