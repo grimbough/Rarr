694c84b70f177d1d73f7831016f0a1260cf02a61
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-21 15:38:27 +0200

    Merge paths tests in a single vector

--- inst/tinytest/test_normalize_path.R
+++ inst/tinytest/test_normalize_path.R
@@ -1,28 +1,24 @@
-windows_paths <- c(
+paths <- c(
+  # Windows paths
   "c:/foo/bar/baz.zarr",
   "d:\\foo\\bar\\baz.zarr",
   "e:\\foo\\bar\\baz.zarr/",
   "z://foo//bar//baz.zarr",
-  "https://s3.foo.com/bar/baz.zarr"
-)
-windows_normalized_paths <- c(
-  "c:/foo/bar/baz.zarr/",
-  "d:/foo/bar/baz.zarr/",
-  "e:/foo/bar/baz.zarr/",
-  "z:/foo/bar/baz.zarr/",
-  "https://s3.foo.com/bar/baz.zarr/"
-)
-
-nix_paths <- c(
+  # Unix-like paths
   "/foo/bar/baz.zarr",
   "foo/bar/baz.zarr",
   "foo/bar/baz.zarr/",
   "../foo/bar/baz.zarr",
   "./foo/bar/baz.zarr",
   ".//foo///bar/baz.zarr",
+  # URLs
   "https://s3.foo.com/bar/baz.zarr"
 )
-nix_normalized_paths <- c(
+normalized_paths <- c(
+  "c:/foo/bar/baz.zarr/",
+  "d:/foo/bar/baz.zarr/",
+  "e:/foo/bar/baz.zarr/",
+  "z:/foo/bar/baz.zarr/",
   "/foo/bar/baz.zarr/",
   "foo/bar/baz.zarr/",
   "foo/bar/baz.zarr/",
@@ -32,16 +28,9 @@ nix_normalized_paths <- c(
   "https://s3.foo.com/bar/baz.zarr/"
 )
 
-for(i in seq_along(windows_paths)) {
+for(i in seq_along(paths)) {
   expect_identical(
-    Rarr:::.normalize_array_path(windows_paths[i]),
-    windows_normalized_paths[i]
-  )
-}
-
-for(i in seq_along(nix_paths)) {
-  expect_identical(
-    Rarr:::.normalize_array_path(nix_paths[i]),
-    nix_normalized_paths[i]
+    Rarr:::.normalize_array_path(paths[i]),
+    normalized_paths[i]
   )
 }
