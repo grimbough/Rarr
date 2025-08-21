a9e016ffec424a84f31df5e54c4c520a4a608a5b
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-21 15:00:11 +0200

    Run all .normalize_array_path() on all platforms

--- inst/tinytest/test_normalize_path.R
+++ inst/tinytest/test_normalize_path.R
@@ -3,6 +3,11 @@ windows_paths <- c(
   "d:\\foo\\bar\\baz.zarr",
   "https://s3.foo.com/bar/baz.zarr"
 )
+windows_normalized_paths <- c(
+  "c:/foo/bar/baz.zarr/",
+  "d:/foo/bar/baz.zarr/",
+  "https://s3.foo.com/bar/baz.zarr/"
+)
 
 nix_paths <- c(
   "/foo/bar/baz.zarr",
@@ -11,33 +16,24 @@ nix_paths <- c(
   "./foo/bar/baz.zarr",
   "https://s3.foo.com/bar/baz.zarr"
 )
+nix_normalized_paths <- c(
+  "/foo/bar/baz.zarr/",
+  "foo/bar/baz.zarr/",
+  "../foo/bar/baz.zarr/",
+  "./foo/bar/baz.zarr/",
+  "https://s3.foo.com/bar/baz.zarr/"
+)
 
-if(.Platform$OS.type == "windows") {
-  
-  expected_output <- c(
-    "c:/foo/bar/baz.zarr/",
-    "d:/foo/bar/baz.zarr/",
-    "https://s3.foo.com/bar/baz.zarr/"
+for(i in seq_along(windows_paths)) {
+  expect_identical(
+    Rarr:::.normalize_array_path(windows_paths[i]),
+    windows_normalized_paths[i]
   )
-  
-  for(i in seq_along(windows_paths)) {
-    expect_identical( Rarr:::.normalize_array_path(windows_paths[i]), expected_output[i] )
-  }
-  
-} else {
-  
-  expected_output <- c(
-    "/foo/bar/baz.zarr/",
-    "foo/bar/baz.zarr/",
-    "../foo/bar/baz.zarr/",
-    "./foo/bar/baz.zarr/",
-    "https://s3.foo.com/bar/baz.zarr/"
-  )
-  
-  for(i in seq_along(nix_paths)) {
-    expect_identical( Rarr:::.normalize_array_path(nix_paths[i]), expected_output[i] )
-  }
-  
 }
 
-
+for(i in seq_along(nix_paths)) {
+  expect_identical(
+    Rarr:::.normalize_array_path(nix_paths[i]),
+    nix_normalized_paths[i]
+  )
+}
