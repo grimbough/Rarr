ea9dd202799c46f66e8e0414c63447e728a4258e
Artur-man <artur-man@hotmail.com>
2025-08-21 17:15:38 +0200

    add tests for writeZarrArray given non-divisible dim/chunk_dim

--- inst/tinytest/test_ZarrArray-class.R
+++ inst/tinytest/test_ZarrArray-class.R
@@ -17,6 +17,13 @@ expect_equal(path(z1), Rarr:::.normalize_array_path(tf1))
 expect_equal(extract_array(z1, index = list(NULL, NULL)), m)
 expect_equal(realize(z1, BACKEND = NULL), m)
 
+# write and read ZarrArray with size non-divisible by chunk_dim
+# should return as usual when chunk_dim = 500,60 is not divisible 
+# by dim = 1000,100
+tf2 <- tempfile(fileext = ".zarr")
+tmp <- writeZarrArray(x = m, zarr_array_path = tf2, chunk_dim = c(500,60))
+expect_equal(m, realize(tmp))
+
 # create ZarrArray with character data type
 m <- matrix("array", ncol = 10, nrow = 10)
 tf2 <- tempfile(fileext = ".zarr")
