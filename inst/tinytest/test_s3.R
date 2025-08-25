e0717daa2847be6ff763982c677a7d2002579192
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-25 15:08:16 +0200

    Add actual test for S3 zarr metadata

--- inst/tinytest/test_s3.R
+++ inst/tinytest/test_s3.R
@@ -14,3 +14,16 @@ if (require(mockery)) {
   ## we expect an "anonymous" credential if nothing is found
   expect_true(s3_client$.internal$config$credentials$anonymous)
 }
+
+test_file_s3_url <- "https://noaa-nwm-retro-v2-zarr-pds.s3.amazonaws.com/feature_id"
+expect_identical(
+  zarr_overview(test_file_s3_url, as_data_frame = TRUE),
+  list2DF(list(
+    path = test_file_s3_url,
+    nchunks = 1,
+    data_type = "int32",
+    compressor = "blosc",
+    dim = list(2729077L),
+    chunk_dim = list(2729077L)
+  ))
+)
