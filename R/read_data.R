ce495b3a5599c9b119406d28c4d10e8dc88ea352
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-26 11:43:58 +0200

    Fix typo

--- R/read_data.R
+++ R/read_data.R
@@ -355,7 +355,7 @@ read_chunk <- function(
   } else if (actual_chunk_size == prod(alt_chunk_dim)) {
     chunk_dim <- alt_chunk_dim
   } else {
-    stop("Decompresed data doesn't match expected chunk size.")
+    stop("Decompressed data doesn't match expected chunk size.")
   }
 
   ## reverse dimensions for column first datasets
