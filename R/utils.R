9a8d7e7ebb0129bdc84128a877c1c36a0d5d1deb
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-27 14:42:42 +0200

    Use readBin() on a connection
    to avoid having to manually move the cursor

--- R/utils.R
+++ R/utils.R
@@ -138,15 +138,14 @@ check_index <- function(index, metadata) {
 }
 
 .readVlenUTF8 <- function(input) {
-  nvalues <- readBin(input, what = "integer", n = 1, size = 4)
+  con <- rawConnection(input)
+  on.exit(close(con))
+  nvalues <- readBin(con, what = "integer", n = 1, size = 4)
   output <- character(length = nvalues)
-  input <- tail(input, -4)
   for (i in seq_len(nvalues)) {
-    nbytes <- readBin(input, what = "integer", n = 1, size = 4)
-    input <- tail(input, -4)
+    nbytes <- readBin(con, what = "integer", n = 1, size = 4)
     if (nbytes > 0) {
-      output[i] <- rawToChar(input[seq_len(nbytes)])
-      input <- tail(input, -nbytes)
+      output[i] <- readChar(con, nchars = nbytes, useBytes = TRUE)
     }
   }
 
