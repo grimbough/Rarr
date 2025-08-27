98245488267e5aaee759d0876c5cd83c16582ea2
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-08-27 10:16:15 -0400

    Add test

--- inst/tinytest/test_read_metadata.R
+++ inst/tinytest/test_read_metadata.R
@@ -13,3 +13,9 @@ expect_equal(dim(df), c(1, 6))
 
 ## write details to screen
 expect_stdout(zarr_overview(zarr_c, as_data_frame = FALSE))
+
+## error when metadata file is not found
+expect_error(
+  zarr_overview("https://objects.eodc.eu:443/e05ab01a9d56408d82ac32d69a5aae2a:202505-s02msil2a/30/products/cpm_v256/S2B_MSIL2A_20250530T101559_N0511_R065_T32TPT_20250530T130924.zarr/measurements/reflectance/r10m"),
+  "The array does not have an associated .zarray metadata file"
+)
