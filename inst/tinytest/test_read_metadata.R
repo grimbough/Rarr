88df3e9102954b9f1d0d59dc13c902c4eb6f9fc9
Sharla Gelfand <sharla.gelfand@gmail.com>
2025-08-28 12:19:01 -0400

    use mockery::stub() instead, explicitly supply s3_client as list()

--- inst/tinytest/test_read_metadata.R
+++ inst/tinytest/test_read_metadata.R
@@ -29,20 +29,18 @@ expect_error(
 )
 
 ## error when metadata file is not found (zarr array on s3)
-with_mocked_bindings(
-  code = {
-    expect_error(
-      zarr_overview("https://objects.eodc.eu:443/e05ab01a9d56408d82ac32d69a5aae2a:202505-s02msil2a/30/products/cpm_v256/S2B_MSIL2A_20250530T101559_N0511_R065_T32TPT_20250530T130924.zarr/measurements/reflectance/r10m/"),
-      "The array does not have an associated .zarray metadata file"
-    )
-  },
-  # Since the s3 client is never actually used (because .s3_object_exists() just
-  # returns FALSE), just need to make sure the client is not NULL, so the
-  # correct flow occurs in read_array_metadata()
-  .create_s3_client = function(path) {
-    list()
-  },
-  .s3_object_exists = function(s3_client, Bucket, Key) {
-    FALSE
-  }
-)
+if (require(mockery)) {
+  # Ensures that .s3_object_exists() just automatically returns FALSE
+  stub(
+    where = Rarr:::read_array_metadata,
+    what = ".s3_object_exists",
+    how = FALSE
+  )
+
+  expect_error(
+    zarr_overview("https://objects.eodc.eu:443/e05ab01a9d56408d82ac32d69a5aae2a:202505-s02msil2a/30/products/cpm_v256/S2B_MSIL2A_20250530T101559_N0511_R065_T32TPT_20250530T130924.zarr/measurements/reflectance/r10m/",
+      s3_client = list()
+    ),
+    "The array does not have an associated .zarray metadata file"
+  )
+}
