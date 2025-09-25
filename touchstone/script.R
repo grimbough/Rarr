90a50696db5d404fdee72e1c5889052c66f585dc
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-09-25 20:21:39 +0200

    Set up touchstone (#51)

---
+++ touchstone/script.R
@@ -0,0 +1,38 @@
+# see `help(run_script, package = 'touchstone')` on how to run this
+# interactively
+
+# TODO OPTIONAL Add directories you want to be available in this file or during the
+# benchmarks.
+# touchstone::pin_assets("some/dir")
+
+# installs branches to benchmark
+touchstone::branch_install()
+
+touchstone::benchmark_run(
+  {
+    library(Rarr)
+  },
+  write_zstd = write_zarr_array(
+    array(1:1e6, dim = c(100, 100, 100)),
+    "zstd.zarr",
+    chunk_dim = c(10, 10, 10),
+    compressor = use_zstd(level = 22)
+  ),
+  n = 50
+)
+
+touchstone::benchmark_run(
+  {
+    library(Rarr)
+    write_zarr_array(
+      array(1:1e6, dim = c(100, 100, 100)),
+      "zstd.zarr",
+      chunk_dim = c(10, 10, 10),
+      compressor = use_zstd(level = 22)
+    )
+  },
+  read_zstd = read_zarr_array("zstd.zarr"),
+  n = 50
+)
+
+touchstone::benchmark_analyze()
