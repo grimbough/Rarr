9c6cf52fb608c38af4694442406ab17534d0f4ce
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-09-30 17:44:44 +0200

    Add test for v3 attributes reading

--- inst/scripts/create_test_data_v3.py
+++ inst/scripts/create_test_data_v3.py
@@ -3,3 +3,12 @@ import numpy as np
 
 x = np.arange(60).reshape(3, 4, 5)
 zarr.save("inst/extdata/zarr_examples/metadata/v3.zarr", x)
+
+z1 = zarr.create_array(
+    store="inst/extdata/zarr_examples/metadata/v3_attr.zarr",
+    shape=(10, 10),
+    chunks=(10, 10),
+    dtype="int32",
+)
+
+z1.attrs["custom"] = "Hello, Zarr!"
