ee35fcbbc55c0d0baed32565d8e13cae5d2ccc62
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-10-31 12:01:24 +0100

    Move .readVlenUTF8() to read_utils.R

--- R/read_utils.R
+++ R/read_utils.R
@@ -46,3 +46,17 @@
   )
   return(converted_chunk)
 }
+
+.readVlenUTF8 <- function(input) {
+  con <- rawConnection(input)
+  on.exit(close(con))
+  nvalues <- readBin(con, what = "integer", n = 1, size = 4)
+  output <- character(length = nvalues)
+  for (i in seq_len(nvalues)) {
+    nbytes <- readBin(con, what = "integer", n = 1, size = 4)
+    output[i] <- readChar(con, nchars = nbytes, useBytes = TRUE)
+  }
+
+  Encoding(output) <- "UTF-8"
+  return(output)
+}
