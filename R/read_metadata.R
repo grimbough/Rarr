9af3fc99d4766b320546a4c251abd4a8ab17e6ef
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-11-12 10:30:49 +0100

    Name codecs in v3 metadata

--- R/read_metadata.R
+++ R/read_metadata.R
@@ -298,6 +298,20 @@ zarr_overview <- function(zarr_array_path, s3_client, as_data_frame = FALSE) {
     ## the parsed version is used each time a chunk is read
     metadata$datatype <- .parse_datatype(metadata$dtype)
     metadata <- .update_fill_value(metadata, metadata$datatype)
+  } else if (metadata$zarr_format == 3) {
+    # We shouldn't have any case where x$name is NULL since the v3 spec states
+    # 'name' MUST be a plain string.
+    names(metadata$codecs) <- vapply(
+      metadata$codecs,
+      function(x) x$name,
+      character(1)
+    )
+  } else {
+    stop(
+      "Unsupported Zarr format version: %s",
+      metadata$zarr_format,
+      call. = FALSE
+    )
   }
 
   return(metadata)
