ced5b9ea17cc32a756b6a793410e98cd5c7142b5
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-11-18 17:45:46 +0100

    Add s3 test in actual test suite

--- tests/testthat/test-s3.R
+++ tests/testthat/test-s3.R
@@ -10,8 +10,27 @@ test_that("Anonymous S3 access is detected properly", {
   )
 })
 
+test_that("Authenticated S3 access works", {
+  skip_if_offline("s3.embl.de")
+
+  withr::with_envvar(
+    c(
+      "AWS_ACCESS_KEY_ID" = "bYUBYVg1AsEreuDgtg5K",
+      "AWS_SECRET_ACCESS_KEY" = "r8FrLXc9dseD6V1P3htsu7ZBzP7Gszsd3sM1G4KX"
+    ),
+    {
+      expect_snapshot(zarr_overview(
+        "https://s3.embl.de/rarr-testing/bzip2.zarr"
+      ))
+      expect_snapshot(read_zarr_array(
+        "https://s3.embl.de/rarr-testing/bzip2.zarr"
+      ))
+    }
+  )
+})
+
 test_that("Denied access errors return clear error messages", {
-  skip_if_offline()
+  skip_if_offline("s3.embl.de")
 
   # This is a real zarr store, but we don't have access
   expect_error(
