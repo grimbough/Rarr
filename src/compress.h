ee6aeda00a687b0118ac398c9d2237091427a536
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-11-22 11:59:33 +0100

    Add or update version number in build files

--- src/compress.h
+++ src/compress.h
@@ -5,7 +5,7 @@
   #include "compression_tools/blosc/lib/blosc-1.21.6/blosc.h"
   #include "compression_tools/blosc/lib/lz4-1.9.2/lz4.h"
   #include "compression_tools/blosc/lib/lz4-1.9.2/lz4hc.h"
-  #include "compression_tools/zstd/zstd.h"
+  #include "compression_tools/zstd-1.5.7/zstd.h"
 #endif
 
 SEXP compress_chunk_BLOSC(SEXP input, SEXP type_size);
