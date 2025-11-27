62ba844a6256f33aac6cb399632b53c0e978fc73
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-11-27 16:31:14 +0100

    Update version number in external files

--- src/compress.h
+++ src/compress.h
@@ -3,8 +3,8 @@
 #ifndef _COMPRESSION_H
   #define _COMPRESSION_H
   #include "compression_tools/blosc/lib/blosc-1.21.6/blosc.h"
-  #include "compression_tools/blosc/lib/lz4-1.9.2/lz4.h"
-  #include "compression_tools/blosc/lib/lz4-1.9.2/lz4hc.h"
+  #include "compression_tools/blosc/lib/lz4-1.10.0/lz4.h"
+  #include "compression_tools/blosc/lib/lz4-1.10.0/lz4hc.h"
   #include "compression_tools/zstd-1.5.7/zstd.h"
 #endif
 
