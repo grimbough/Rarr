38ef050ed8bb56ebb6241e5b7d1262edd134cba0
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-12-04 19:22:46 +0100

    Remove obsolete short circuit in transpose codec
    The no-op case is now handled is .configure_codecs() by not configuring this codec

--- R/codecs_array_array.R
+++ R/codecs_array_array.R
@@ -1,8 +1,5 @@
 # -- Transpose -------------------
 codec_transpose_encode <- function(array, indices = seq_along(dim(array))) {
-  if (identical(indices, seq_along(dim(array)))) {
-    return(array)
-  }
   inv_indices <- order(indices)
   array <- aperm(array, inv_indices)
   dim(array) <- dim(array)[inv_indices]
@@ -11,9 +8,6 @@ codec_transpose_encode <- function(array, indices = seq_along(dim(array))) {
 }
 
 codec_transpose_decode <- function(array, indices = seq_along(dim(array))) {
-  if (identical(indices, seq_along(dim(array)))) {
-    return(array)
-  }
   dim(array) <- dim(array)[indices]
   array <- aperm(array, indices)
 
