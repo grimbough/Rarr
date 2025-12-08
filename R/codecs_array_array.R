3b9813399fbc6b3ad0783a6dd112fa8519ebfe0a
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-12-08 09:09:45 +0100

    Add FIXME note about extra copy

--- R/codecs_array_array.R
+++ R/codecs_array_array.R
@@ -1,5 +1,6 @@
 # -- Transpose -------------------
 codec_transpose_encode <- function(array, indices = seq_along(dim(array))) {
+  # FIXME: we do two copies here (aperm and dim assignment) which can be expensive for large arrays
   inv_indices <- order(indices)
   array <- aperm(array, inv_indices)
   dim(array) <- dim(array)[inv_indices]
