bc6439894c08f4894f9e02df9587c321616167eb
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2026-01-15 17:19:48 +0100

    Implement spec version of bytes_codec_encode

--- tests/testthat/test-codecs.R
+++ tests/testthat/test-codecs.R
@@ -24,22 +24,3 @@ test_that("codec: transpose", {
     x
   )
 })
-
-test_that("codec: endian", {
-  x <- runif(100)
-
-  raw_le <- writeBin(x, con = raw(), size = 8, endian = "little")
-  raw_be <- writeBin(x, con = raw(), size = 8, endian = "big")
-
-  if (.Platform$endian == "little") {
-    expect_identical(
-      raw_le |> codec_bytes_encode("big", 8),
-      raw_be
-    )
-  } else {
-    expect_identical(
-      raw_be |> codec_bytes_encode("little", 8),
-      raw_le
-    )
-  }
-})
