f634dfdff7781067af31bf8914c1206c0257ca40
Artur-man <artur-man@hotmail.com>
2026-01-19 13:21:16 +0100

    wrap with as.list instead

--- R/write_metadata.R
+++ R/write_metadata.R
@@ -16,8 +16,8 @@
 
   zarray <- list(
     # the spec states these need to be json arrays, so we need to avoid auto_unboxing
-    shape = array(array_shape),
-    chunks = array(chunk_shape),
+    shape = as.list(array_shape),
+    chunks = as.list(chunk_shape),
     dtype = data_type,
     fill_value = fill_value,
     dimension_separator = dimension_separator,
