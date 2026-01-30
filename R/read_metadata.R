c0bb9949a59e0b742624e38e10c27fd790ed2f61
Artur-man <artur-man@hotmail.com>
2026-01-30 21:38:09 +0100

    drop default simplifyVector = TRUE in fromJSON

--- R/read_metadata.R
+++ R/read_metadata.R
@@ -482,7 +482,7 @@ read_zarr_attributes <- function(zarr_path, s3_client) {
       Key = parsed_url$object
     )
 
-    zattrs <- fromJSON(rawToChar(s3_object$Body))
+    zattrs <- fromJSON(rawToChar(s3_object$Body), simplifyVector = FALSE)
   } else {
     zattrs <- read_json(attribute_path)
   }
