ae2f14e6d7bae53efb5b9c8fcd607a0d862b49a2
Artür Manukyan <artur-man@hotmail.com>
2026-01-31 17:26:18 +0100

    add comment on `simplifyVector=FALSE`
    Co-authored-by: Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>

--- R/read_metadata.R
+++ R/read_metadata.R
@@ -482,6 +482,8 @@ read_zarr_attributes <- function(zarr_path, s3_client) {
       Key = parsed_url$object
     )
 
+   # simplifyVector = FALSE is used for consistency with read_json(), 
+   # used on local files.
     zattrs <- fromJSON(rawToChar(s3_object$Body), simplifyVector = FALSE)
   } else {
     zattrs <- read_json(attribute_path)
