f8f44a40f06d156b8b8c81702d6cc2bc8906e3e1
Hugo Gruson <git@hugogruson.fr>
2026-03-12 15:20:02 +0100

    Add stdbool header

--- src/Rarr.h
+++ src/Rarr.h
@@ -7,6 +7,7 @@
 #include <Rversion.h> 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
   
 /* From https://cran.r-project.org/doc/manuals/r-devel/R-exts.html#Some-backports-1 */
 #if R_VERSION < R_Version(4, 6, 0)
