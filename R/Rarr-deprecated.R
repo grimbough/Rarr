7610fcf1ac8d497f5c1e10722dc881b13431b1a2
Hugo Gruson <git@hugogruson.fr>
2026-03-16 14:59:09 +0100

    Use a slightly smoother deprecation process

---
+++ R/Rarr-deprecated.R
@@ -0,0 +1,23 @@
+ZarrArray <- function(...) {
+  .Deprecated(
+    "ZarrArray",
+    package = "ZarrArray",
+    msg = paste(
+      "The functions related to the DelayedArray backend have moved",
+      "to the dedicated ZarrArray package (https://github.com/Bioconductor/ZarrArray)",
+      "under the same name."
+    )
+  )
+}
+
+writeZarrArray <- function(...) {
+  .Deprecated(
+    "writeZarrArray",
+    package = "ZarrArray",
+    msg = paste(
+      "The functions related to the DelayedArray backend have moved",
+      "to the dedicated ZarrArray package (https://github.com/Bioconductor/ZarrArray)",
+      "under the same name."
+    )
+  )
+}
