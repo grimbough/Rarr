207033e4eab460fed6cf0cda610430e9baeefc0c
Hugo Gruson <git@hugogruson.fr>
2026-03-16 09:49:10 +0100

    Try making CLI wrapper arg parsing more efficient

--- inst/scripts/Rarr-cli.R
+++ inst/scripts/Rarr-cli.R
@@ -4,13 +4,13 @@ args <- commandArgs(trailingOnly = TRUE)
 
 # simple --key=val parser
 options <- args |>
-  grepv(pattern = "^--", x = _) |>
-  strsplit("=") |>
-  do.call(rbind, args = _) |>
-  as.data.frame() |>
-  setNames(c("arg", "val"))
+  Filter(\(x) startsWith(x, "--"), x = _) |>
+  strsplit("=", fixed = TRUE)
 
-opt_list <- setNames(options$val, gsub("^--", "", options$arg))
+opt_list <- setNames(
+  vapply(options, `[`, 2, FUN.VALUE = character(1)),
+  gsub("^--", "", vapply(options, `[`, 1, FUN.VALUE = character(1)))
+)
 
 res <- Rarr::read_zarr_array(
   zarr_array_path = opt_list[["array_path"]]
