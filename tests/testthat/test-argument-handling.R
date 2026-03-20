27f235a990cb9d0afe71d981fd4c7258c04793d8
Hugo Gruson <git@hugogruson.fr>
2026-03-20 19:17:13 +0100

    Add config options to use_blosc()

--- tests/testthat/test-argument-handling.R
+++ tests/testthat/test-argument-handling.R
@@ -1,6 +1,16 @@
-test_that("use_blosc() validates cname argument", {
-  expect_error(
-    use_blosc(cname = "foo"),
-    "cname argument must be one of"
+test_that("use_blosc() accepts all configurable options", {
+  compressor <- use_blosc(
+    cname = "blosclz",
+    clevel = 9L,
+    shuffle = "bitshuffle",
+    typesize = 8L,
+    blocksize = 0L
   )
+
+  expect_identical(compressor$id, "blosc")
+  expect_identical(compressor$cname, "blosclz")
+  expect_identical(compressor$clevel, 9L)
+  expect_identical(compressor$shuffle, 2L)
+  expect_identical(compressor$typesize, 8L)
+  expect_identical(compressor$blocksize, 0L)
 })
