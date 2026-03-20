5c8e0a0947085fe6a5c8f5b24bb2860ca626755b
Hugo Gruson <git@hugogruson.fr>
2026-03-20 20:02:02 +0100

    Write shufflemode as text in metadata

--- tests/testthat/test-argument-handling.R
+++ tests/testthat/test-argument-handling.R
@@ -10,7 +10,7 @@ test_that("use_blosc() accepts all configurable options", {
   expect_identical(compressor$id, "blosc")
   expect_identical(compressor$cname, "blosclz")
   expect_identical(compressor$clevel, 9L)
-  expect_identical(compressor$shuffle, 2L)
+  expect_identical(compressor$shuffle, "bitshuffle")
   expect_identical(compressor$typesize, 8L)
   expect_identical(compressor$blocksize, 0L)
 })
