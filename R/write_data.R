82f556ba36414151f8d16fbc781f8575c88615ab
Hugo Gruson <git@hugogruson.fr>
2026-04-07 18:25:59 +0200

    Add lossy save warnings to update_zarr_array()

--- R/write_data.R
+++ R/write_data.R
@@ -450,6 +450,35 @@ update_zarr_array <- function(zarr_array_path, x, index) {
     stop("New data is not of the same type as the existing array.")
   }
 
+  same_type_lower_bytesize <- metadata$data_type %in%
+    c("int8", "int16", "float32")
+  lower_bytesize_type <- storage.mode(x) == "double" &&
+    metadata$data_type == "float32"
+
+  can_overflow <- same_type_lower_bytesize || lower_bytesize_type
+  if (can_overflow) {
+    x <- .truncate_overflow(x, metadata$datatype$nbytes)
+  }
+
+  if (anyNA(x)) {
+    if (metadata$data_type == "bool") {
+      warning(
+        "Zarr native 'bool' data type does not support NA values. ",
+        "NA values will be converted to FALSE. ",
+        "To preserve NA values, use 'uint8' datatype in `write_zarr_array()` and `as.logical()` after reading.",
+        call. = FALSE
+      )
+    }
+    if (metadata$data_type == "string") {
+      warning(
+        "Zarr 'string' data type does not support NA values. ",
+        "NA values will be converted to empty strings. ",
+        "To preserve NA values, use 'unicode' datatype in `write_zarr_array()`.",
+        call. = FALSE
+      )
+    }
+  }
+
   metadata$configured_encoders <- .configure_codecs(
     codecs = metadata$codecs,
     operation = "encode"
