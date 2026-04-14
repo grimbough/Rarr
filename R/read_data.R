72f1d3e4f0ea137dbcf6fda09c7f8db316761ae2
Hugo Gruson <git@hugogruson.fr>
2026-04-14 18:52:25 +0200

    Set s3_client default to NULL
    Instead of missing. So NULL/missing values can be more easily passed down from dependencies

--- R/read_data.R
+++ R/read_data.R
@@ -40,10 +40,10 @@
 #' }
 #'
 #' @export
-read_zarr_array <- function(zarr_array_path, index, s3_client) {
+read_zarr_array <- function(zarr_array_path, index, s3_client = NULL) {
   zarr_array_path <- .normalize_array_path(zarr_array_path)
   ## determine if this is a local or S3 array
-  if (missing(s3_client)) {
+  if (is.null(s3_client)) {
     s3_client <- .create_s3_client(path = zarr_array_path)
   }
 
