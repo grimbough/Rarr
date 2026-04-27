73ef26e94b07f7bd8249de1cf6d0e52e4f09791a
Hugo Gruson <git@hugogruson.fr>
2026-04-27 22:32:00 +0200

    Use rlang::inject() to extract chunks

--- R/write_data.R
+++ R/write_data.R
@@ -331,11 +331,7 @@ write_zarr_array <- function(
     )
   }
 
-  chunk_in_mem <- eval(str2lang(.create_extract_call(
-    x_name = "x",
-    idx_name = "idx_in_array",
-    idx_length = length(idx_in_array)
-  )))
+  chunk_in_mem <- .extract_chunk(x, idx_in_array)
 
   ## if a chunk overlaps the edge of the array, most implementations assume we
   ## still write the content to disk.  Seems wasteful, but we fail many tests
@@ -569,11 +565,7 @@ update_zarr_array <- function(zarr_array_path, x, index) {
 
   ## extract the new values from x and insert them into the chunk
   # nolint next: object_usage_linter.
-  y <- eval(str2lang(.create_extract_call(
-    "x",
-    "idx_in_x",
-    length(idx_in_x)
-  )))
+  y <- .extract_chunk(x, idx_in_x)
   cmd <- .create_replace_call(
     "chunk_in_mem",
     "idx_in_chunk",
