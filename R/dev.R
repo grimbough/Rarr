e505b1f79c5a8815162f4edf1295d67788b72c37
Hugo Gruson <git@hugogruson.fr>
2026-04-29 14:24:47 +0200

    Wrap linter in requireNamespace()

--- R/dev.R
+++ R/dev.R
@@ -4,12 +4,14 @@
 #'
 #' @return A linter function to be used with \code{lintr::lint()}.
 #' @noRd
-from_json_simplify_linter <- lintr::make_linter_from_function_xpath(
-  function_names = "fromJSON",
-  xpath = "parent::expr[not(SYMBOL_SUB[
-    text() = 'simplifyVector' and
-    following-sibling::expr[1]/NUM_CONST[text() = 'FALSE']
-  ])]",
-  lint_message = "fromJSON() should always be called with simplifyVector = FALSE.",
-  type = "warning"
-)
+if (requireNamespace("lintr", quietly = TRUE)) {
+  from_json_simplify_linter <- lintr::make_linter_from_function_xpath(
+    function_names = "fromJSON",
+    xpath = "parent::expr[not(SYMBOL_SUB[
+      text() = 'simplifyVector' and
+      following-sibling::expr[1]/NUM_CONST[text() = 'FALSE']
+    ])]",
+    lint_message = "fromJSON() should always be called with simplifyVector = FALSE.",
+    type = "warning"
+  )
+}
