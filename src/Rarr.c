1a3fa9134a489ecd30b1e4625351e1d80237c693
Hugo Gruson <git@hugogruson.fr>
2026-05-07 16:09:06 +0200

    Remove now unused C code

--- src/Rarr.c
+++ src/Rarr.c
@@ -1,7 +1,6 @@
 #include "Rarr.h"
 #include "decompress.h"
 #include "compress.h"
-#include "type_conversion.h"
 #include "codec_vlen-utf8.h"
 
 static const R_CallMethodDef callMethods[] = {
@@ -12,12 +11,7 @@ static const R_CallMethodDef callMethods[] = {
   {"compress_chunk_BLOSC", (DL_FUNC) &compress_chunk_BLOSC, 6},
   {"compress_chunk_LZ4", (DL_FUNC) &compress_chunk_LZ4, 1},
   {"compress_chunk_ZSTD", (DL_FUNC) &compress_chunk_ZSTD, 2},
-  
-  {"type_convert_int", (DL_FUNC) &type_convert_int, 2},
-  {"type_convert_uint", (DL_FUNC) &type_convert_uint, 2},
-  {"type_convert_float", (DL_FUNC) &type_convert_float, 2},
-  {"type_convert_bool", (DL_FUNC) &type_convert_bool, 2},
-  {"type_convert_string", (DL_FUNC) &type_convert_string, 2},
+
   {"codec_vlen_utf8_decode_c", (DL_FUNC) &codec_vlen_utf8_decode_c, 2},
   {NULL, NULL, 0}
 };
--- src/float16_conversion.h
+++
@@ -1,3 +0,0 @@
-#include "Rarr.h"
-
-double float16_to_float64(uint16_t float16_value);
