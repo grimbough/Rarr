7c14983741de789dc167d82261d8a208107727cb
Hugo Gruson <git@hugogruson.fr>
2026-06-06 11:40:58 +0200

    Error on unsupported codec

--- tests/testthat/test-codecs.R
+++ tests/testthat/test-codecs.R
@@ -24,3 +24,41 @@ test_that("codec: transpose", {
     x
   )
 })
+
+test_that("unsupported codec", {
+  invalid_codec <- withr::local_tempfile(fileext = ".zarr")
+  dir.create(invalid_codec)
+  file.copy(
+    system.file(
+      "extdata",
+      "zarr_examples",
+      "column-first",
+      "boolean_v3.zarr",
+      package = "Rarr"
+    ),
+    invalid_codec,
+    recursive = TRUE
+  )
+  meta <- jsonlite::read_json(
+    file.path(invalid_codec, "boolean_v3.zarr", "zarr.json")
+  )
+  meta$codecs <- c(
+    meta$codecs,
+    list(
+      list(
+        "name" = "unsupported_codec",
+        "configuration" = list()
+      )
+    )
+  )
+  jsonlite::write_json(
+    meta,
+    file.path(invalid_codec, "boolean_v3.zarr", "zarr.json"),
+    auto_unbox = TRUE,
+    pretty = TRUE
+  )
+  expect_error(
+    Rarr::read_zarr_array(file.path(invalid_codec, "boolean_v3.zarr")),
+    "not supported: unsupported_codec"
+  )
+})
