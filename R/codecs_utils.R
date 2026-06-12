c368a9ce0bc51d156c8a5a136f8adab90cd96723
Hugo Gruson <git@hugogruson.fr>
2026-06-12 15:18:24 +0200

    Make bytes/bytes codec arg simpler

--- R/codecs_utils.R
+++ R/codecs_utils.R
@@ -71,8 +71,8 @@
         candidate_codec
       )
       func_name <- paste("codec", candidate_codec, operation, sep = "_")
-      bytes_bytes_env[[candidate_codec]] <- eval(bquote(function(...) {
-        do.call(.(func_name), c(list(...), .(cfg)))
+      bytes_bytes_env[[candidate_codec]] <- eval(bquote(function(bytes) {
+        do.call(.(func_name), c(list(bytes), .(cfg)))
       }))
     }
   }
