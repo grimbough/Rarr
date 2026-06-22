caa8b816461bcc030aff37bfb74c6d73eaf422c8
Hugo Gruson <git@hugogruson.fr>
2026-06-22 16:01:03 +0200

    Skip more S3 tests where necessary

--- tests/testthat/test-s3.R
+++ tests/testthat/test-s3.R
@@ -12,6 +12,7 @@ test_that("Anonymous S3 access is detected properly", {
 
 test_that("Authenticated S3 access works", {
   skip_if_offline("s3.embl.de")
+  skip_on_bioc()
 
   withr::with_envvar(
     c(
@@ -31,6 +32,7 @@ test_that("Authenticated S3 access works", {
 
 test_that("Denied access errors return clear error messages", {
   skip_if_offline("s3.embl.de")
+  skip_on_bioc()
 
   # This is a real zarr store, but we don't have access
   expect_error(
@@ -40,6 +42,9 @@ test_that("Denied access errors return clear error messages", {
 })
 
 test_that("JSON metadata is not unboxed", {
+  skip_if_offline("livingobjects.ebi.ac.uk")
+  skip_on_bioc()
+
   # We don't want to save the metadata and test this locally. It has to be tested on a real S3 store because it relates
   # to a bug in the code branch.
   # See https://github.com/Huber-group-EMBL/Rarr/pull/170
