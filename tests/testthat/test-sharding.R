93f3fb6a9adbefd9eebf4c10c4fbfa53db6f7ddd
Hugo Gruson <git@hugogruson.fr>
2026-06-23 18:45:36 +0200

    Add tests for sharded dataset

---
+++ tests/testthat/test-sharding.R
@@ -0,0 +1,23 @@
+test_that("read sharded files", {
+  sharded <- system.file(
+    "extdata",
+    "zarr_examples",
+    "sharding",
+    "int32_sharded_v3.zarr",
+    package = "Rarr"
+  )
+
+  arr <- read_zarr_array(sharded) |>
+    expect_no_condition() |>
+    expect_shape(dim = c(30L, 20L, 10L)) |>
+    expect_type("integer")
+
+  expect_identical(
+    arr[1L, , 1L],
+    seq_len(20L)
+  )
+  expect_identical(
+    arr[, 1L, 1L],
+    rep_len(1L, 30L)
+  )
+})
