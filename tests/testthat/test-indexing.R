37166395ce2d10aaabff93938ae9368b428fb5da
Hugo Gruson <git@hugogruson.fr>
2026-07-16 21:27:40 +0200

    Add tests for out of order indexing

--- tests/testthat/test-indexing.R
+++ tests/testthat/test-indexing.R
@@ -57,3 +57,38 @@ test_that("integer(0) indexing works correctly", {
   )
   expect_identical(res, integer(0), ignore_attr = "dim")
 })
+
+test_that("out of order indices are handled correctly", {
+  zarr_v2 <- system.file(
+    package = "Rarr",
+    "extdata",
+    "zarr_examples",
+    "column-first",
+    "int32.zarr"
+  )
+  # Non-compact indices
+  expect_no_condition(
+    res1 <- read_zarr_array(zarr_v2, list(c(3, 1), c(2, 1), c(1, 3)))
+  )
+  expect_shape(res1, dim = c(2, 2, 2))
+  expect_no_condition(
+    res2 <- read_zarr_array(zarr_v2, list(c(1, 3), c(1, 2), c(3, 1)))
+  )
+  expect_identical(
+    res1,
+    res2[c(2, 1), c(2, 1), c(2, 1)]
+  )
+
+  # Compact indice ranges
+  expect_no_condition(
+    res3 <- read_zarr_array(zarr_v2, list(3:1, 2:1, 1:3))
+  )
+  expect_shape(res3, dim = c(3, 2, 3))
+  expect_no_condition(
+    res4 <- read_zarr_array(zarr_v2, list(1:3, 1:2, 3:1))
+  )
+  expect_identical(
+    res3,
+    res4[c(3, 2, 1), c(2, 1), c(3, 2, 1)]
+  )
+})
