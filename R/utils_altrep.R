f2dfad85c1c67cba1d3b98ab9dd88f45ebccd7b4
Hugo Gruson <git@hugogruson.fr>
2026-07-16 21:26:52 +0200

    Fix out of order indexing

--- R/utils_altrep.R
+++ R/utils_altrep.R
@@ -5,11 +5,7 @@ is.compact <- function(x) {
 reindex <- function(x, from = 1L, to = 0L) {
   offset <- from - to
   if (is.compact(x)) {
-    if (is.unsorted(x)) {
-      res <- (min(x) - offset):(max(x) - offset)
-    } else {
-      res <- (x[1L] - offset):(x[length(x)] - offset)
-    }
+    res <- (x[1L] - offset):(x[length(x)] - offset)
   } else {
     res <- x - offset
   }
