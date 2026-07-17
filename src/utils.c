cd055bf962dd67a61fd68e58b95618846b4c4fc3
Hugo Gruson <git@hugogruson.fr>
2026-07-17 23:08:57 +0200

    First draft for chop_vec utility function

--- src/utils.c
+++ src/utils.c
@@ -6,3 +6,20 @@ SEXP is_compact(SEXP x) {
   }
   return ScalarLogical(FALSE);
 }
+
+SEXP chop_vec(SEXP x, SEXP _size) {
+  R_xlen_t n = XLENGTH(x);
+  int size = INTEGER(_size)[0];
+  R_xlen_t m = n / size;
+
+  SEXP result = PROTECT(allocVector(VECSXP, m));
+  SEXP el;
+
+  for (R_xlen_t i = 0; i < m; i++) {
+    el = allocVector(TYPEOF(x), size);
+    memcpy(INTEGER(el), INTEGER(x) + i * size, size * sizeof(int));
+    SET_VECTOR_ELT(result, i, el);
+  }
+  UNPROTECT(1);
+  return result;
+}
